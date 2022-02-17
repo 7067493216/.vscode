08e093985a91c754a6743f25dc1ec4da7211e300
7067493216 <mdshamshad10feb@gmail.com>
2022-02-18 02:31:47 +0530

    js notes

--- index.js
+++ index.js
@@ -1,43 +1,64 @@
-var a=10;
-console.log(a);
+//var a=10;
+//console.log(a);
 
-var a="hello";
-console.log(a);
-console.log(typeof a);
+//var a="hello";
+//console.log(a);
+//console.log(typeof a);
 
-var a="hello";
-console.log(typeof a);
+//var a="hello";
+//console.log(typeof a);
 
-a=true;
-console.log(a);
-console.log(typeof a);
+//a=true;
+////console.log(a);
+//console.log(typeof a);
 
-a=null;
-console.log(a);
-console.log(typeof a);
+//a=null;
+//console.log(a);
+//console.log(typeof a);
 
-var num=10;
-console.log(num);
-var float=2.4;
-console.log(float);
+//var num=10;
+//console.log(num);
+//var float=2.4;
+//console.log(float);
 
 //string
 //'',"",``
 //single quote,double quote'backticks
 
-var str=`a`;
-console.log(str);
-str="how you doing";
-console.log(str);
-str= "who are you\nand what are doing here";
-console.log(str);
+//var str=`a`;
+//console.log(str);
+//str="how you doing";
+//console.log(str);
+//str= "who are you\nand what are doing here";
+//console.log(str);
 
-var b=`you know
-who i am`;
-console.log(b);
+//var b=`you know
+//who i am`;
+//console.log(b);
 
-var num=100;
-console.log(`half of ${num} is ${num/2}`);
+//var num=100;
+//console.log(`half of ${num} is ${num/2}`);
+var num =10;
+for(var i=0; i<10;i++){
+    if(i%2==0){
+    console.log(i);
+}
+}
+// var: in var variable can be reinialize as well as redecleared.
 
-var a="hello"
-console.log(`print ${a} and${a/2}`);
+// let: in let variable can be initialize but variable can not be redecleared.
+
+// const: in const variable neither be reinitialized nor be redecleared.
+var num=10
+let j=30;
+for(var i=0;i<num;i++){
+    let j=33
+    if(i%2==0){
+        console.log(i);
+        console.log(num);
+        var hello =20000
+    }
+    console.log(j);
+}
+console.log(j);
+console.log(hello);
