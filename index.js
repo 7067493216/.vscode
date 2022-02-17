5bcd7667bf4b52347dfd258f6ba5577b74ab7847
7067493216 <mdshamshad10feb@gmail.com>
2022-02-17 12:47:01 +0530

    javascript notes

---
+++ index.js
@@ -0,0 +1,40 @@
+var a=10;
+console.log(a);
+
+var a="hello";
+console.log(a);
+console.log(typeof a);
+
+var a="hello";
+console.log(typeof a);
+
+a=true;
+console.log(a);
+console.log(typeof a);
+
+a=null;
+console.log(a);
+console.log(typeof a);
+
+var num=10;
+console.log(num);
+var float=2.4;
+console.log(float);
+
+//string
+//'',"",``
+//single quote,double quote'backticks
+
+var str=`a`;
+console.log(str);
+str="how you doing";
+console.log(str);
+str= "who are you\nand what are doing here";
+console.log(str);
+
+var b=`you know
+who i am`;
+console.log(b);
+
+var num=100;
+console.log(`half of ${num} is ${num/2}`);
